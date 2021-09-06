8b8e2680e008cb4925b72188b34ddcb9a1ac01e1
forresttanaka <ftanaka@mac.com>
2021-09-05 17:28:21 -0700

    Initial commit

---
+++ libs/svg-icons.js
@@ -0,0 +1,13 @@
+export const Minus = ({ css }) => (
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 1024 1024" className={css}>
+        <g><rect y="422" width="1024" height="181"/></g>
+    </svg>
+);
+
+export const Plus = ({ css }) => (
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" className={css}>
+        <g>
+            <polygon points="602.5,422 602.5,0.5 421.5,0.5 421.5,422 0,422 0,467 0,558 0,603 421.5,603 421.5,1024.5 602.5,1024.5 602.5,603 1024,603 1024,558 1024,467 1024,422" />
+        </g>
+    </svg>
+);
---
+++ pages/demos/facets-hide.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import * as SvgIcon from '../../libs/svg-icons';
+
+
+const termsAssayType = [
+    {
+        field: 'dna-binding',
+        title: 'DNA Binding',
+        count: 7746,
+    },
+    {
+        field: 'transcription',
+        title: 'Transcription',
+        count: 2592,
+    },
+    {
+        field: 'dna-accessibility',
+        title: 'DNA Accessibility',
+        count: 1561,
+    },
+];
+
+const termsAssayTitle = [
+    {
+        field: 'tf-chip-seq',
+        title: 'TF ChIP-seq',
+        count: 3316,
+    },
+    {
+        field: 'histone-chip-seq',
+        title: 'Histone ChIP-seq',
+        count: 3316,
+    },
+    {
+        field: 'dnase-seq',
+        title: 'DNase-seq',
+        count: 1201,
+    },
+];
+
+const demoFacets = [
+    {
+        field: 'assay-type',
+        title: 'Assay Type',
+        terms: termsAssayType,
+    },
+    {
+        field: 'assay-title',
+        title: 'Assay Title',
+        terms: termsAssayTitle,
+    }
+];
+
+const Term = ({ term }) => {
+    return (
+        <div className="term">
+            <div className="term__title">{term.title}</div>
+            <div className="term__count">{term.count}</div>
+        </div>
+    );
+};
+
+const TermList = ({ terms, isExpanded }) => {
+    return (
+        <div className={`term-list${isExpanded ? ' term-list--expanded' : ''}`}>
+            {terms.map(term => {
+                return <Term key={term.field} term={term} />;
+            })}
+        </div>
+    );
+};
+
+const FacetTrigger = ({ facet, isExpanded, onClick }) => (
+    <button type="button" className="facet-trigger" onClick={() => onClick(facet.field)}>
+        <div className="facet-trigger__title">{facet.title}</div>
+        <div className="facet-trigger__indicator">
+            {isExpanded
+                ? <SvgIcon.Minus css="facet-trigger__indicator-icon" />
+                : <SvgIcon.Plus css="facet-trigger__indicator-icon" />
+            }
+        </div>
+    </button>
+);
+
+const Facet = ({ facet, isExpanded, onClick }) => {
+    return (
+        <div className="facet" key={facet.field}>
+            <FacetTrigger facet={facet} isExpanded={isExpanded} onClick={onClick} />
+            <TermList terms={facet.terms} isExpanded={isExpanded} />
+        </div>
+    );
+};
+
+const initExpandedFacets = (facets) => {
+    return facets.reduce((accExpandedFacets, facet) => {
+        accExpandedFacets[facet.field] = false;
+        return accExpandedFacets;
+    }, {});
+};
+
+const FacetList = () => {
+    const facetsToRender = demoFacets;
+    const [expandedFacets, setExpandedFacets] = React.useState(() => initExpandedFacets(facetsToRender));
+
+    const facetTriggerHandler = (facetField) => {
+        setExpandedFacets({
+            ...expandedFacets,
+            [facetField]: !expandedFacets[facetField],
+        });
+    };
+
+    return (
+        <div className="facet-list">
+            {facetsToRender.map(facet => {
+                return <Facet key={facet.field} facet={facet} isExpanded={expandedFacets[facet.field]} onClick={facetTriggerHandler} />
+            })}
+        </div>
+    )
+};
+
+export default FacetList;
